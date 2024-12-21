cb44c6ccd02e647fc64bdd03bcb95f8ed3415d6c
shoji yuki <youkey2929@gmail.com>
2024-12-21 22:33:47 +0900

    初回コミット

--- src/main/java/practice/ShoppingSystem/controller/LoginController.java
+++ src/main/java/practice/ShoppingSystem/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import practice.ShoppingSystem.constant.ErrorMessageConst;
 import practice.ShoppingSystem.form.LoginForm;
 import practice.ShoppingSystem.service.LoginService;
